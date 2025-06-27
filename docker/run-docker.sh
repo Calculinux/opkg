fa774f9d9b69766e03154ae6f677aa047ffb5470
Alex Stewart <alex.stewart@emerson.com>
2025-06-27 12:16:56 -0400

    docker: run container as local user
    Running the opkg-dev container as root tends to create permissions
    issues when building distribution content.
    
    Run the container as the local user, to match ownership information
    between the host and container.
    
    Move the container workspace mount point to /usr/local/src, since that
    is a more standard build location for foreign project sources.
    
    Signed-off-by: Alex Stewart <alex.stewart@emerson.com>

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -3,9 +3,12 @@
 set -ex
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PROJECT_ROOT=$(realpath "${SCRIPT_PATH}/..")
 
 docker build $SCRIPT_PATH -t opkg-dev
 docker run -it \
-    -v $(pwd):/root/ \
-    -e HISTFILE=/root/.bash_history \
+    -v "${PROJECT_ROOT}":/usr/local/src/opkg/:rw \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -u $(id -u):$(id -g) \
     opkg-dev
